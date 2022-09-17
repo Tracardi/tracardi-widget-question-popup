73b2dca901e11e590e9d73515225a6502003d20a
risto <risto.kowaczewski@gmail.com>
2022-09-18 00:19:35 +0200

    styling the widget

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import "regenerator-runtime/runtime";
 import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 import Snackbar from "@material-ui/core/Snackbar";
 import SnackbarContent from "@material-ui/core/SnackbarContent";
 
@@ -11,63 +10,74 @@ function App({domElement}) {
 
     const [open, setOpen] = React.useState(true);
 
-    const API_URL = "http://localhost:8686"; // hard-coded for now
-    const sourceId = domElement.getAttribute("source-id");
-    const sessionId = domElement.getAttribute("session-id");
-    const leftBtnText = domElement.getAttribute("left-button-text");
-    const rightBtnText = domElement.getAttribute("right-button-text");
-    const popupTitle = domElement.getAttribute("popup-title");
-    const content = domElement.getAttribute("content");
-    const posHorizontal = domElement.getAttribute("horizontal-position");
-    const posVertical = domElement.getAttribute("vertical-position");
-    const popupLifetime = parseInt(domElement.getAttribute("popup-lifetime")) * 1000;
-    const theme = domElement.getAttribute("theme");
-    const eventType = domElement.getAttribute("event-type");
-    const saveEvent = domElement.getAttribute("save-event");
-    const profileId = domElement.getAttribute("profile-id");
-
+    const api_url = domElement.getAttribute("data-api-url") || "http://localhost:8686";
+    const sourceId = domElement.getAttribute("data-source-id");
+    const sessionId = domElement.getAttribute("data-session-id");
+    const leftBtnType = domElement.getAttribute("data-left-button-type") || "text";
+    const rightBtnType = domElement.getAttribute("data-right-button-type") || "text";
+    const leftBtnText = domElement.getAttribute("data-left-button-text");
+    const rightBtnText = domElement.getAttribute("data-right-button-text");
+    const popupTitle = domElement.getAttribute("data-popup-title");
+    const content = domElement.getAttribute("data-content");
+    const posHorizontal = domElement.getAttribute("data-horizontal-position");
+    const posVertical = domElement.getAttribute("data-vertical-position");
+    const popupLifetime = parseInt(domElement.getAttribute("data-popup-lifetime")) * 1000;
+    const eventType = domElement.getAttribute("data-event-type");
+    const saveEvent = domElement.getAttribute("data-save-event");
+    const profileId = domElement.getAttribute("data-profile-id");
+    const boxBgColor = domElement.getAttribute("data-box-bg-color") || "white";
+    const textColor = domElement.getAttribute("data-text-color") || "black";
+    const titleSize = domElement.getAttribute("data-title-size") || "22px";
+    const questionSize = domElement.getAttribute("data-question-size") || "15px";
+    const boxElevation = domElement.getAttribute("data-box-elevation") || "1";
+    const boxMaxWidth = domElement.getAttribute("data-box-max_width") || "200px";
 
     const sendEvent = async (answer) => {
 
-        const response = await fetch(`${API_URL}/track`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                "source": {
-                    "id": sourceId
+        try {
+            const response = await fetch(`${api_url}/track`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
                 },
-                "session": {
-                    "id": sessionId
-                },
-                "metadata": {
-                    "ip": "254.23.12.188"
-                },
-                "profile": {
-                    "id": profileId
-                },
-                "context": {},
-                "properties": {},
-                "events": [
-                    {
-                        "type": eventType,
-                        "properties": {
-                            "answer": answer
-                        },
-                        "options": {
-                            "saveEvent": saveEvent === 'yes'
+                body: JSON.stringify({
+                    "source": {
+                        "id": sourceId
+                    },
+                    "session": {
+                        "id": sessionId
+                    },
+                    "metadata": {
+                        "ip": "254.23.12.188"
+                    },
+                    "profile": {
+                        "id": profileId
+                    },
+                    "context": {},
+                    "properties": {},
+                    "events": [
+                        {
+                            "type": eventType,
+                            "properties": {
+                                "answer": answer
+                            },
+                            "options": {
+                                "saveEvent": saveEvent === 'yes'
+                            }
                         }
-                    }
-                ],
-                "options": {}
+                    ],
+                    "options": {}
+                })
             })
-        })
 
-        const data = await response.json()
-        console.log(data);
+            const data = await response.json()
+            console.log(data);
 
-        setOpen(false);
+        } catch (e) {
+            console.error(e)
+        } finally {
+            setOpen(false);
+        }
     }
 
     const handleClose = (event, reason) => {
@@ -76,57 +86,53 @@ function App({domElement}) {
         }
     };
 
-    return (
-        <Snackbar
-            anchorOrigin={{vertical: posVertical, horizontal: posHorizontal}}
-            open={open}
-            onClose={handleClose}
-            autoHideDuration={popupLifetime}
-        >
-            <SnackbarContent
-                style={{
-                    backgroundColor: theme === "dark" ? "#343434" : "#ffffff",
-                    display: "flex",
-                    flexDirection: "row",
-                    justifyContent: "center"
-                }}
-                message={
-                    <>
-                        <Box
-                            sx={{
-                                display: "flex",
-                                flexDirection: "column",
-                                justifyContent: "center",
-                                alignItems: "center"
-                            }}
-                        >
-                            <Box sx={{color: theme === "dark" ? "#ffffff" : "#343434"}}>
-                                <Typography variant="h5">{popupTitle}</Typography>
-                            </Box>
-                            <Box sx={{color: theme === "dark" ? "#ffffff" : "#343434"}}>
-                                <Typography variant="p">{content}</Typography>
-                            </Box>
-                            <Box
-                                sx={{
-                                    alignSelf: "center",
-                                    margin: "10px",
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    gap: "20px",
-                                    color: theme === "dark" ? "#ffffff" : "#343434"
-                                }}
-                            >
-                                <Button variant="outlined" onClick={() => sendEvent(leftBtnText)}
-                                        color="inherit">{leftBtnText}</Button>
-                                <Button variant="outlined" onClick={() => sendEvent(rightBtnText)}
-                                        color="inherit">{rightBtnText}</Button>
-                            </Box>
-                        </Box>
-                    </>
-                }
-            />
-        </Snackbar>
-    );
+    return <Snackbar
+        anchorOrigin={{vertical: posVertical, horizontal: posHorizontal}}
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={popupLifetime}
+    >
+        <SnackbarContent
+            style={{
+                backgroundColor: boxBgColor,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                maxWidth: boxMaxWidth,
+                padding: "0px 20px"
+            }}
+            elevation={boxElevation}
+            message={
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        color: textColor
+                    }}
+                >
+                    <div style={{fontSize: titleSize, padding: "10px 0 0 6px"}}>{popupTitle}</div>
+                    <p style={{overflow: "scroll", maxHeight: 103, fontSize: questionSize}}>{content}</p>
+                    <Box
+                        sx={{
+                            alignSelf: "center",
+                            margin: "10px",
+                            display: "flex",
+                            flexDirection: "row",
+                            gap: "20px",
+                            color: textColor
+                        }}
+                    >
+                        <Button variant={leftBtnType} onClick={() => sendEvent(leftBtnText)}
+                                color="inherit">{leftBtnText}</Button>
+                        <Button variant={rightBtnType} onClick={() => sendEvent(rightBtnText)}
+                                color="inherit">{rightBtnText}</Button>
+                    </Box>
+                </Box>
+            }
+        />
+    </Snackbar>;
 
 }
 
